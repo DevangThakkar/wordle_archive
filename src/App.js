2bcae8d7166d02e17d483a812ef9436640ae0db5
devang <devang01121995@gmail.com>
2022-01-09 14:37:19 -0500

    change title size

--- src/App.js
+++ src/App.js
@@ -318,7 +318,7 @@ function App() {
             <button type="button" onClick={() => setSettingsModalIsOpen(true)}>
               <Settings />
             </button>
-            <h1 className="flex-1 text-center text-xl xxs:text-2xl -mr-6 sm:text-4xl tracking-wide font-bold font-og">
+            <h1 className="flex-1 text-center text-l xxs:text-xl -mr-6 sm:text-3xl tracking-wide font-bold font-og">
               WORDLE ARCHIVE {day}
             </h1>
             <button type="button" onClick={() => setInfoModalIsOpen(true)}>
